1fd50c2a56433aaa6aac6c6ed95ecdc1c3c44b66
Ayshee <152168245+Ayshee782@users.noreply.github.com>
2025-03-07 22:03:01 +0600

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,16 @@
+#dictionary are used to store data values in key:value pair.
+#They are unordered ,mutable & don't allow duplicate key.
+
+a={
+    "b":"name",
+    "c":"555",
+    "d":[56,89,76],
+}
+print(a)
+print(a["b"])
+a["b"]="Ayshee"
+print(a["b"])
+
+#empty dict
+null_a={}
+print(null_a)
