bd99442762aeb1c7698382d6c779a4d34626472b
Ayshee <152168245+Ayshee782@users.noreply.github.com>
2025-05-02 21:36:36 +0600

    Add files via upload

---
+++ dic 1.py
@@ -0,0 +1,49 @@
+CSE={
+    "PULS": {
+        "NAME":"RAKIB SIR",
+        "PHONE NUMBER":"****",
+        "SAMISTER":"6TH",
+        "SECTION":"E",
+        "YEAR":"2025",
+    },
+
+    "CA":{
+        "NAME":"DANASTAN",
+        "PHONE":"******",
+        "SAMISTER":"6TH",
+        "SECTION":"E",
+        "YEAR":"2025",
+    },
+    "TOC":{
+        "NAME":"DIP SIR",
+        "PHONE":"***",
+        "SAMISTER":"6TH",
+        "SACTION":"E",
+        "YEAR":"2025",
+    },
+    "DC":{
+        "NAME":"ISTAYAK SIR",
+        "PHONE":"*****",
+        "SAMISTER":"6TH",
+        "SACTION":"E",
+        "YEAR":"2025",
+    },
+    "MATH V":{
+        "NAME":"YOUSUF SIR",
+        "PHONE":"******",
+        "SAMISTER":"6TH",
+        "SACTION":"E",
+        "YEAR":"2025",
+    },
+}
+BBA={
+    "Business English":"MAM",
+    "Business Communication ":"SIR",
+    "Principles of Marketing":"MAM",
+}
+LLB={
+    "Banking and Securities Law":"SIR",
+    "Law of International Trade":"MAM",
+    "Comparative Constitutional Law":"SIR",
+}
+print (CSE)
